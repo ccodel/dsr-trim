f577d312cef3349b5cc70a3b33526b5016866e4a
Marijn Heule <mheule@Marijns-Air.lan>
2024-03-11 17:06:10 -0400

    improved sr proofs

--- php/php-sr.c
+++ php/php-sr.c
@@ -3,23 +3,19 @@
 
 int main (int argc, char** argv) {
   int p = atoi (argv[1]);
-  int q = p+1;
 
   for (int k = 1; k < p; k++) {
     int i;
     for (i = p; i >= k; i--) {
-      if (i == k)
-        printf ("-%i %i -%i %i -%i ", i*p + k, (i-1)*p + k, i*p + k, (i-1)*p + k, i*p + k);
-      else
-        printf ("-%i -%i %i -%i ", i*p + k, i*p + k, (i-1)*p + k, i*p + k);
-      for (int j = k+1; j < q; j++)
+      printf ("-%i -%i %i -%i ", i*p + k, i*p + k, (i-1)*p + k, i*p + k);
+      for (int j = k+1; j <= p; j++)
         printf ("%i %i %i %i ", (i-1)*p + j, i*p + j, i*p + j, (i-1)*p + j);
       printf ("0\n");
     }
-    printf ("-%i 0\n", (i+1)*p + k);
     printf ("%i 0\n", (i)*p + k);
     for (int j = k+1; j <= p; j++)
       printf ("-%i 0\n", i*p + j);
   }
 
+  printf ("0\n");
 }
