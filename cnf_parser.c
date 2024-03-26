31d22aee45e5224de6c80323dc0fcfb6079f9d2f
Cayden Codel <ccodel@andrew.cmu.edu>
2024-03-26 16:56:46 -0400

    feat: Handle repeated lits and tautologies in CNF parsing

--- cnf_parser.c
+++ cnf_parser.c
@@ -34,7 +34,6 @@ void parse_cnf(const char *filename) {
         PRINT_ERR_AND_EXIT_IF(res < 0, "Read error on problem line.");
         break;
       default:
-        printf("c Character found was %c\n", c);
         PRINT_ERR_AND_EXIT("Char wasn't a comment or a problem line.");
     }
   }
@@ -45,34 +44,70 @@ void parse_cnf(const char *filename) {
 
   init_global_data(num_clauses, num_vars);
 
-  // Allocate memory
-  /*
-  witness_buf_size = num_lits * 2;
-  witness_buf = xmalloc(witness_buf_size * sizeof(int));
-
-  hints_buf_size = num_cnf_clauses * 2;
-  hints_buf = xmalloc(hints_buf_size * sizeof(int));
-  */
-
   // Now parse in the rest of the CNF file
+  // We assume that no more comment lines can appear, and will error if a non-number is parsed
   while (formula_size < num_clauses) {
-    // TODO: Assume no more comment lines?
     int parsed_lit = 0, clause_size = 0;
     do {
       READ_NUMERICAL_TOKEN(res, f, &parsed_lit);
-      if (parsed_lit == 0) {
-        // Sort the literals in the clause before adding it
-        // Because no clause should be a tautology, the absolute values are distinct
-        int size = get_clause_size(formula_size);
-        void *start = ((void *) get_clause_start(formula_size));
-        qsort(start, size, sizeof(int), absintcompare);
-        // TODO: Reject tautologies?
-        insert_clause();
-      } else {
+      if (parsed_lit != 0) {
         int lit = FROM_DIMACS_LIT(parsed_lit);
         insert_lit(lit);
+        clause_size++;
       }
     } while (parsed_lit != 0);
+
+    if (clause_size == 0) {
+      derived_empty_clause = 1;
+      break;
+    } else if (clause_size == 1) {
+      insert_clause();
+      continue;
+    }
+
+    // Now we sort the literals in the clause
+    // Then, we efficiently remove duplicate literals and detect tautologies
+    // Tautological clauses are added but are deleted, leaving a "hole"
+    int *write_ptr, *read_ptr = get_clause_start(formula_size);
+    qsort((void *) read_ptr, clause_size, sizeof(int), intcmp);
+    int is_tautology = 0;
+    int skipped_lits = 0;
+    int lit, next_lit = read_ptr[0];
+
+    for (int i = 0; i < clause_size - 1; i++) {
+      read_ptr++;
+      lit = next_lit;
+      next_lit = *read_ptr;
+
+      if (lit == next_lit) {
+        skipped_lits++;
+        if (skipped_lits == 1) {
+          write_ptr = read_ptr - 1;
+        }
+      } else if (lit == NEGATE_LIT(next_lit)) {
+        is_tautology = 1;
+        break;
+      } else if (skipped_lits > 0) {
+        *write_ptr = lit;
+        write_ptr++;
+      }
+    }
+
+    // Write the last literal
+    // This can "write" a duplicate, if the duplicates ended the clause
+    // But the writing is "erased" when the lits_db_size is adjusted below
+    if (skipped_lits > 0) {
+      *write_ptr = next_lit;
+    }
+
+    if (!is_tautology) {
+      lits_db_size -= skipped_lits; // Update the clause's size before adding
+      insert_clause();
+    } else {
+      lits_db_size -= clause_size;
+      insert_clause();
+      delete_clause(formula_size - 1);
+    }
   }
 
   fclose(f);
@@ -80,6 +115,10 @@ void parse_cnf(const char *filename) {
 
 void print_cnf(void) {
   for (int c = 0; c < formula_size; c++) {
+    if (is_clause_deleted(c)) {
+      continue;
+    }
+
     int *clause_ptr = get_clause_start(c);
     int *clause_end = get_clause_start(c + 1);
     for (; clause_ptr < clause_end; clause_ptr++) {
