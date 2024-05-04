0c09b3bd79d651c64aaef5ab3f5349c475056883
Cayden Codel <ccodel@andrew.cmu.edu>
2024-05-04 14:29:28 -0400

    fix: rename dsr-trim in gen.sh

--- php/gen.sh
+++ php/gen.sh
@@ -2,13 +2,15 @@
 
 # Usage: ./gen.sh <start> <end>
 # Generates PHP .cnf/.sr files in the range of the input.
-# If `sr-trim` exists in the parent directory, it will also generate .lsr files.
+# If `dsr-trim` exists in the parent directory, it will also generate .lsr files.
 # The script must be run with `pwd` in the same directory as `gen.sh`.
 
 if ! [ -f ./gen.sh ]; then
   echo "Error: Run this script in the same directory as gen.sh" >&2
   exit 1
 fi
+
+echo "Generating php files"
 
 mkdir -p cnf
 mkdir -p sr
@@ -19,9 +21,10 @@ done
 
 # If sr-trim exists in the parent directory, run it on each file in cnf/
 
-if [ -f ../sr-trim ]; then
+if [ -f ../dsr-trim ]; then
+  echo "../dsr-trim detected, generating .lsr files"
   mkdir -p lsr
   for i in $(seq $1 $2); do
-    ../sr-trim cnf/php$i.cnf sr/php$i.sr lsr/php$i.lsr
+    ../dsr-trim cnf/php$i.cnf sr/php$i.sr lsr/php$i.lsr
   done
 fi
