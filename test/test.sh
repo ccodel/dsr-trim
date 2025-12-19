66340fcfcf3e5a2c2a3b84e7866b3743211c23e1
Michael McLoughlin <mcloughlin@cmu.edu>
2025-12-19 21:59:21 +0000

    parallel --halt

--- test/test.sh
+++ test/test.sh
@@ -3,4 +3,4 @@
 set -euo pipefail
 
 cd "$(dirname "${BASH_SOURCE[0]}")"
-grep -Ev '^#' benchmarks.in | parallel --no-run-if-empty ./test_gbd_benchmark.sh :::: -
+grep -Ev '^#' benchmarks.in | parallel --no-run-if-empty --halt 'soon,fail=1' ./test_gbd_benchmark.sh :::: -
